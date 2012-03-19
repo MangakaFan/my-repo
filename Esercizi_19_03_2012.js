da3679c77317662c53b95ba08ce7d7da7e43279a
Andrea Patrizio <andrea.patrizio@gmail.com>
2012-03-19 17:26:15 +0100

    aggiunte

--- Esercizi_19_03_2012.js
+++ Esercizi_19_03_2012.js
@@ -33,3 +33,56 @@ var comp = function(args) {
 		return args.reduce(comp2);
 	};
 };
+
+// ritorna l'array dei risultati delle singole funzioni (con for)
+var cons = function(args) {
+	return function(x) {
+		var a = new Array();
+		for (var i = 0; i < args.length; i++)
+			a.push(args[i](x));
+		return a;
+	};
+};
+
+
+// ritorna l'array dei risultati delle singole funzioni (con map)
+var cons = function(args) {
+	return function(x) {
+		return args.map(function(item) {
+			return item(x);
+		});
+	};
+};
+
+//distribuisce a sinistra (con for)
+var distl = function(args) {
+	var a = new Array();
+	for (var i = 0; i < args[1].length; i++) {
+		var b = new Array();
+		b.concat(args[0]);
+		b.concat(args[1][i]);
+		a.push(b);
+	}
+	return a;
+};
+
+
+//distribuisce a sinistra (con map)
+var distl = function(args) {
+	var value = args[0];
+	var array = args[1];
+	return array.map(function(item) {
+		return [value, item];
+	});
+};
+
+// trasposta di una matrice (scambio righe per colonne)
+var trans = function(args) {
+	var a = new Array();
+	for (var i = 0; i < args[0].length; i++) {
+		var b = new Array();
+		for (var j = 0; j < args.length; j++)
+			b.push(args[i][j]);
+		a.push(b);
+	}			
+};
