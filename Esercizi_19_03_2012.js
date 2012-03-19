272cd86ea0695dd8c9fd816d94e3c49ab552689e
Andrea Patrizio <andrea.patrizio@gmail.com>
2012-03-19 16:37:45 +0100

    aggiunte

--- Esercizi_19_03_2012.js
+++ Esercizi_19_03_2012.js
@@ -13,3 +13,12 @@ var comp2 = function(args) {
 		return args[0](args[1](x));
 	}
 }
+
+var comp = function(args) {
+	return function(x) {
+		var result = x;
+		for (var j = args.length-1; j >= 0; j--)
+			result = args[j](result);
+		return result;
+	}
+}
